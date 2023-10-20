648af242e4ef8c8976da4c6a66a9c48bd0042a3c
Arkty <vaitron228@mail.ru>
2023-10-20 16:53:40 +0300

    Add CustomLogger

---
+++ Assets/CodeBase/Infrastructure/Services/Logger/CustomLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeBase.Infrastructure.Services.Logger;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.Services
+{
+    public class CustomLogger : ICustomLogger
+    {
+        public void Log(object message) => 
+            Debug.Log($"{message}");
+
+
+        public void LogWarning(object message) => 
+            Debug.LogWarning($"{message}");
+
+
+        public void LogError(Exception exception) => 
+            throw exception;
+    }
+}
